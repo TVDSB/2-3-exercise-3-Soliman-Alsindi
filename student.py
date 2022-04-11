adf2438fbfc980f0a5975de8685825ee05d1b0f6
Soliman-Alsindi <100424208+Soliman-Alsindi@users.noreply.github.com>
2022-04-11 09:03:32 -0400

    Update student.py

--- student.py
+++ student.py
@@ -1,6 +1,19 @@
 def main():
     #your code goes here
-    print("0")
+   # UNIT 2 ACTIVITY 3.2
+    num = int(input("What Is The Number\n"))
+
+    if num % 15 == 0:
+      print("FizzBuzz")
+
+    elif num % 5 == 0:
+       print("buzz")
+
+    elif num % 3 == 0:
+      print ("Fizz")
+
+    else:
+      print(num)
 
 if __name__ =='__main__':
     main()
